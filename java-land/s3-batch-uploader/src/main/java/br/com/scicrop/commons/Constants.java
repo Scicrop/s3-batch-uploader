4629810805b8e5c24ee555ad806b8a9b70e34b5a
José Damico <jd.comment@gmail.com>
2018-09-14 13:57:57 -0300

    added transfer data

--- java-land/s3-batch-uploader/src/main/java/br/com/scicrop/commons/Constants.java
+++ java-land/s3-batch-uploader/src/main/java/br/com/scicrop/commons/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
 	public static final String LOG_PATH = TEMP_PATH+APP_NAME+".log";
 	public static final String POSIX_DEFAULT_PROPERTIES_FILE_PATH = "/etc/"+APP_NAME+"/"+APP_NAME+".conf";
 	public static final String WIN_DEFAULT_PROPERTIES_FILE_PATH   = APP_NAME+"\\"+APP_NAME+".conf";
-	public static final String APP_VERSION = "0.4";
+	public static final String APP_VERSION = "0.5";
 	
 }
